dfab0a5500fde8b1a2fdf3f3e4a769c965d726fd
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-03-15 21:58:56 +0000

    Updated my Javascript to supply accurate date and times for both my locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,30 @@
-let londonElement = document.querySelector("#london");
-let londonDateElement = londonElement.querySelector(".date");
-let londonTimeElement = londonElement.querySelector("#time-1");
+function updateTime() {
+  // London
 
-let londonDtElement = moment();
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector("#time-1");
 
-londonDateElement.innerHTML = londonDtElement.format("MMMM Do YYYY");
-londonTimeElement.innerHTML = londonDtElement.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+  let londonDtElement = moment.tz("Europe/London");
+
+  londonDateElement.innerHTML = londonDtElement.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonDtElement.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  //   San-Juan
+
+  let sanJuanElement = document.querySelector("#san-juan");
+  let sanJuanDateElement = sanJuanElement.querySelector(".date");
+  let sanJuanTimeElement = sanJuanElement.querySelector("#time-2");
+
+  let sanJuanDtElement = moment.tz("America/Puerto_Rico");
+
+  sanJuanDateElement.innerHTML = sanJuanDtElement.format("MMMM Do YYYY");
+  sanJuanTimeElement.innerHTML = sanJuanDtElement.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
