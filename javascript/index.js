6af434136206a2b981f059c2793b79acb96362df
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-03-15 21:32:00 +0000

    Starting the basic javascript to include the accurate date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,5 @@
-
+let londonElement = document.querySelector("#london");
+let londonDateElement = londonElement.querySelector(".date");
+let londonTimeElement = londonElement.querySelector(".time-1");
+londonDateElement.innerHTML = "March 24th 2024";
+londonTimeElement.innerHTML = "9:30:45 <small>AM</small>";
