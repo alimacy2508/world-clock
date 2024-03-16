4b5ca05f38eb92f0c74cd55931cad0efaa676af4
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-03-16 19:42:19 +0000

    Tidied up the display of the dropdown city name when chosen
    Also repaired bug with original cities function that appeared when a dropdown city replaced them

--- javascript/index.js
+++ javascript/index.js
@@ -2,28 +2,34 @@ function updateTime() {
   // London
 
   let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector(".date");
-  let londonTimeElement = londonElement.querySelector(".time");
 
-  let londonDtElement = moment.tz("Europe/London");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
 
-  londonDateElement.innerHTML = londonDtElement.format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonDtElement.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    let londonDtElement = moment.tz("Europe/London");
+
+    londonDateElement.innerHTML = londonDtElement.format("MMMM Do YYYY");
+    londonTimeElement.innerHTML = londonDtElement.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //   San-Juan
 
   let sanJuanElement = document.querySelector("#san-juan");
-  let sanJuanDateElement = sanJuanElement.querySelector(".date");
-  let sanJuanTimeElement = sanJuanElement.querySelector(".time");
 
-  let sanJuanDtElement = moment.tz("America/Puerto_Rico");
+  if (sanJuanElement) {
+    let sanJuanDateElement = sanJuanElement.querySelector(".date");
+    let sanJuanTimeElement = sanJuanElement.querySelector(".time");
 
-  sanJuanDateElement.innerHTML = sanJuanDtElement.format("MMMM Do YYYY");
-  sanJuanTimeElement.innerHTML = sanJuanDtElement.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    let sanJuanDtElement = moment.tz("America/Puerto_Rico");
+
+    sanJuanDateElement.innerHTML = sanJuanDtElement.format("MMMM Do YYYY");
+    sanJuanTimeElement.innerHTML = sanJuanDtElement.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 updateTime();
@@ -32,12 +38,13 @@ setInterval(updateTime, 1000);
 function updateCity(event) {
   let citiesTimezone = event.target.value;
   let cityTime = moment().tz(citiesTimezone);
+  let cityName = citiesTimezone.replace("_", " ").split("/")[1];
   let citiesElement = document.querySelector("#cities");
 
   citiesElement.innerHTML = `
    <div class="city">
   <div>
-            <h2>${citiesTimezone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
